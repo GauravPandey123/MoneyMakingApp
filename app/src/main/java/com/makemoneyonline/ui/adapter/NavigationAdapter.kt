747eb36d206848d22c94b7a3a209dc911c9cf6ae
Gaurav_pandey <gaurav.iec12@gmail.com>
2020-01-13 17:49:28 +0530

    minor changes are done

--- app/src/main/java/com/makemoneyonline/ui/adapter/NavigationAdapter.kt
+++ app/src/main/java/com/makemoneyonline/ui/adapter/NavigationAdapter.kt
@@ -2,9 +2,10 @@ package com.makemoneyonline.ui.adapter
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class NavigationAdapter(val context : Context{
+class NavigationAdapter(val context : Context) : RecyclerView.Adapter<NavigationAdapter.NavigationViewHolder>(){
 
 
 
@@ -15,5 +16,17 @@ class NavigationAdapter(val context : Context{
 
     inner class NavigationViewHolder(val view : View) : RecyclerView.ViewHolder(view){
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NavigationViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: NavigationViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
